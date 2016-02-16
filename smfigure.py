4f0d09d42bc236b446c7acd57f30035453e23c1f
Becky Smethurst <becky@macbook-air.nat.physics.ox.ac.uk>
2016-02-16 14:42:05 +0000

    smfigure updates

--- smfigure.py
+++ smfigure.py
@@ -1,6 +1,6 @@
 #github.com/rjsmethurst/prefig
 """
-    An awesome plotting object to make any plot poster or presentation ready in the colour of your choice!
+    An awesome plotting object to make any matplotlib plot look like it's been made with SuperMongo. 
     
     R. J. Smethurst 
 """
@@ -9,27 +9,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import colorConverter
+import os
+
+os.environ['PATH'] = os.environ['PATH'] + ':/usr/texbin'
 
 class SMfigure(plt.Figure):
     """
         A class that can replace the 'plt.figure' python plotting command to create plots with defaults similar to SuperMongo plots.
         """
-    def __init__(self):
-        
-        if edgecolor == 'w':
-            col = colorConverter.to_rgba_array(plt.rcParams['axes.color_cycle'])[:,:-1]
-            inv_col = (256-(256*col))/256.0
-            plt.rc('axes', color_cycle = list(inv_col))
-        
-        plt.rc('figure', figsize=(4,4), facecolor='none', edgecolor='none')
-        plt.rc('savefig', dpi=300, facecolor='none', edgecolor='none', frameon='False')
-        f = {'family':serif, 'size':100%}
-        plt.rc('font', **f)
-        plt.rc('text', color=fontcol)
-        plt.rc('axes', labelsize='x-large', edgecolor=edgecolor, labelcolor=labelcolor, facecolor='none', linewidth=3)
-        plt.rc('xtick', labelsize='large', color=fontcol)
-        plt.rc('ytick', labelsize='large', color=fontcol)
-        plt.rc('lines', markersize=8, linewidth=2)
+
+    params =   {'font.size' : 11,
+                'text.usetex' : True,
+                'xtick.major.size': 8,
+                'ytick.major.size': 8,
+                'xtick.minor.size': 3,
+                'ytick.minor.size': 3,
+                }
+    plt.rcParams.update(params) 
+
+    plt.rc('figure', figsize=(4,4), facecolor='none', edgecolor='none', autolayout=True)
+    plt.rc('path', simplify=True)
+    plt.rc('font', family='monospace', monospace=['Nimbus Mono L','Courier New', 'Andale Mono', 'Courier', 'Bitstream Vera Sans Mono',  'Fixed', 'Terminal', 'monospace'])
+    plt.rc('axes', labelsize='large', facecolor='none', linewidth=0.7, color_cycle = ['k', 'r', 'g', 'b', 'c', 'm', 'y'])
+    plt.rc('xtick', labelsize='medium')
+    plt.rc('ytick', labelsize='medium')
+    plt.rc('lines', markersize=4, linewidth=1, markeredgewidth=0.2)
+    plt.rc('legend', numpoints=1, frameon=False, handletextpad=0.3, scatterpoints=1, handlelength=2, handleheight=0.1)
+    plt.rc('savefig', dpi=300, facecolor='none', edgecolor='none', frameon='False')
+
 
 
         
